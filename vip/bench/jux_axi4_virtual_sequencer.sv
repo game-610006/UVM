9af0122b79eabad58688304a216563b10cffc19a
game-610006 <68074497+game-610006@users.noreply.github.com>
2021-03-16 16:46:01 +0800

    Create jux_axi4_virtual_sequencer.sv

---
+++ vip/bench/jux_axi4_virtual_sequencer.sv
@@ -0,0 +1,10 @@
+class jux_axi4_virtual_sequencer extends uvm_sequencer;
+`uvm_component_utils(jux_axi4_virtual_sequencer)
+
+function new(input string name="jux_axi4_virtual_sequencer", input uvm_component parent=null);
+	super.new(name, parent);
+//	$display("========in virtual_seq function new=======\n");
+endfunction
+
+
+endclass

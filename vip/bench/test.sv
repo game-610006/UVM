94f07d1ebb6969a6cc2962a8c92f0aa29cad07de
game-610006 <68074497+game-610006@users.noreply.github.com>
2021-03-16 16:48:54 +0800

    Update test.sv

--- vip/bench/test.sv
+++ vip/bench/test.sv
@@ -15,3 +15,19 @@ task body();
 	`uvm_info(tID,"run end test end",UVM_LOW);
 endtask : body
 endclass : test_vseq
+
+class test extends jux_axi4_test;
+`uvm_component_utils(test)
+
+function new(string name, uvm_component parent);
+	super.new(name,parent);
+endfunction : new
+
+virtual function void build_phase(uvm_phase phase);
+	super.build_phase(phase);
+	`uvm_info(get_type_name(),"RUN build phase RUNNING",UVM_MEDIUM)
+	uvm_config_wrapper::set(this, "env.virtual_sequencer.run_phase",
+		"default_sequence", test_vseq::type_id::get());
+	
+endfunction:build_phase
+endclass : test 
